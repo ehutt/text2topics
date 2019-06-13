82a7bed084f989139eb69d599683808c7cdb8750
Elizabeth Hutton <elizabethhutton@Elizabeths-MacBook-Pro.local>
2019-06-13 13:24:29 -0700

    add dataClean fxn

--- source/dataClean.py
+++ source/dataClean.py
@@ -6,8 +6,6 @@ Created on Tue Jun 11 14:50:34 2019
 @author: elizabethhutton
 """
 
-#PROTOTYPE
-
 
 #Notes: 
 #consider identifying POS (at least for names) to avoid case problems
@@ -25,31 +23,37 @@ from nltk.stem.porter import PorterStemmer
 nltk.download('punkt')
 nltk.download('stopwords')
 
-#load case data from pickle 
-filename1 = 'documents_raw.pkl'
-raw_docs = pd.read_pickle(filename1)
-raw_text = list(raw_docs['text'])
+
+def dataClean(raw_file,clean_file,STEM):
+
+    #load data from pickle 
+    raw_docs = pd.read_pickle(raw_file)
+    raw_text = list(raw_docs['text'])
 
 
-stop_words = set(stopwords.words('english'))
-porter = PorterStemmer()
-clean_docs=list() 
+    stop_words = set(stopwords.words('english'))
+    porter = PorterStemmer()
+    clean_docs=list() 
 
-for doc in raw_text: 
-    #split into tokens
-    tokens = word_tokenize(doc)
-    #convert to lower case, remove punctuation (and numbers)
-    words = [word.lower() for word in tokens if word.isalpha()]
-    words_nostops = [w for w in words if not w in stop_words]
-    #stemming words potentially removes important semantic info 
-    words_stemmed = [porter.stem(w) for w in words_nostops]
-    clean_docs.append(words_stemmed)
+    for doc in raw_text: 
+        #split into tokens
+        tokens = word_tokenize(doc)
+        #convert to lower case, remove punctuation (and numbers)
+        words = [word.lower() for word in tokens if word.isalpha()]
+        words = [w for w in words if not w in stop_words]
+        if STEM==True: 
+            words = [porter.stem(w) for w in words]
+        clean_docs.append(words)
+    
+    with open(clean_file, 'wb') as f:
+        pickle.dump(clean_docs,f)
+    
+    return 
+    
 
-
-filename2 = 'documents_clean.pkl' 
-with open(filename2, 'wb') as f:
-    pickle.dump(clean_docs,f)
-
+raw_file = 'documents_raw.pkl'
+clean_file = 'documents_clean.pkl' 
+dataClean(raw_file,clean_file,STEM=True)
 
     
     
