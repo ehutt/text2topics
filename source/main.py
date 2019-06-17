ed7153e0935de04c86b8acbf71fcd52550845485
Elizabeth Hutton <elizabethhutton@Elizabeths-MacBook-Pro.local>
2019-06-17 16:49:58 -0700

    add argparse functionality

--- source/main.py
+++ source/main.py
@@ -11,27 +11,38 @@ import pandas as pd
 from dataIngest import dataDownload
 from dataClean import dataClean, saveClean
 from model_lda import LDA, makeDict, saveDict,makeDTM,saveDTM, saveLDA
-
-#download packages if first time running 
-nltk.download('punkt')
-nltk.download('stopwords')
-
+import argparse
 
 #starting with only AK cases from 2008
 api_AK = 'https://api.case.law/v1/cases/?jurisdiction=ark&decision_date_min=2008-12-31&full_case=true'
 
+parser = argparse.ArgumentParser(description='Text 2 Topics with LDA')
+parser.add_argument('N_TOPICS', type=int,help='Number of topics to identify (Default=3)')
+parser.add_argument('N_PASS', type=int,help='Number of model iterations (Default=10)')
+
+#optional arguments
+parser.add_argument('--url', type=str, default=api_AK, help='URL for data download. (Default CAP API)')
+                
+args = parser.parse_args()
+print(args)
+
+#download packages if first time running 
+#nltk.download('punkt')
+#nltk.download('stopwords')
+
 #define path names
 raw_file = 'data/documents_raw.pkl'
 clean_file = 'data/documents_clean.pkl' 
-model_file = 'model.gensim' 
-DTM_file = 'DTM.pkl'
-dict_file = 'dictionary.gensim'
+model_file = 'data/model.gensim' 
+DTM_file = 'data/DTM.pkl'
+dict_file = 'data/dictionary.gensim'
 
 
 #process and save data
-dataDownload(api_AK,raw_file)
+url = args.url
+dataDownload(url,raw_file)
 
-clean_docs = dataClean(raw_file,STEM=True)
+clean_docs = dataClean(raw_file,FILTER= True,STEM=False)
 #saveClean(clean_docs,clean_file)
 
 #make dictionary from documents
@@ -42,13 +53,13 @@ dictionary = makeDict(clean_docs)
 DTM = makeDTM(clean_docs,dictionary)
 #saveDTM(DTM, DTM_file)
 
-#set num topics and LDA iterations
-N_TOPICS = 4
-N_PASS = 20
+##set num topics and LDA iterations
+N_TOPICS = args.N_TOPICS
+N_PASS = args.N_PASS
 
 #construct LDA model
-#ldamodel = LDA(DTM,dictionary, N_TOPICS, N_PASS)
-#saveLDA(ldamodel,model_file)
+ldamodel = LDA(DTM,dictionary, N_TOPICS, N_PASS)
+saveLDA(ldamodel,model_file)
 
 
 
