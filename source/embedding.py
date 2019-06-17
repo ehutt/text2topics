dbec38dc91bb85af532075a9c8777004cd82452a
Elizabeth Hutton <elizabethhutton@Elizabeths-MacBook-Pro.local>
2019-06-17 11:11:51 -0700

    fix fxn args in main

---
+++ source/embedding.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 17 11:02:21 2019
+
+@author: elizabethhutton
+"""
+
+from gensim.models import Word2Vec
+from nltk.cluster import KMeansClusterer
+import nltk
+import numpy as np 
+
+
