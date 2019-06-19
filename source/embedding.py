8d2a447b9bce5ac93c35a5db4f0185fb9498f50f
Elizabeth Hutton <elizabethhutton@Elizabeths-MacBook-Pro.local>
2019-06-18 18:20:50 -0700

    download spacy pretrained models

--- source/embedding.py
+++ source/embedding.py
@@ -6,9 +6,23 @@ Created on Mon Jun 17 11:02:21 2019
 @author: elizabethhutton
 """
 
-from gensim.models import Word2Vec
-from nltk.cluster import KMeansClusterer
-import nltk
-import numpy as np 
+import spacy 
+from dataClean import dataClean
+import pandas as pd
 
+
+#load pretrained embedding model (GloVe)
+glove = spacy.load('en_vectors_web_lg')
+nlp = spacy.load('en_core_web_lg')
+
+raw_file = 'data/documents_raw.pkl'
+clean_docs = dataClean(raw_file,FILTER=True,STEM=False)
+
+
+vectorized_docs = list() 
+
+for i, doc in enumerate(clean_docs):
+    doc_vect = pd.DataFrame() 
+    for j, word in enumerate(doc):
+        v = glove(word).vector 
 
